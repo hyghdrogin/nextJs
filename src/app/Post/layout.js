4457b2d7493e1b689637c4e187823c054eb5cfcd
Hyghdrogin <omokewt@gmail.com>
2023-05-29 14:18:52 +0100

    Learning NextJs Dynamic Routing

---
+++ src/app/Post/layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const layout = () => {
+
+    return (
+        <div>Post layout</div>
+    )
+}
+
+export default layout;
---
+++ src/app/Post/new/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const page = () => {
+
+    return (
+        <div>New Post Page</div>
+    )
+}
+
+export default page;
